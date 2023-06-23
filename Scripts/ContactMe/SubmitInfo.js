3dae45709435b26554ddd21f8328c5b3e231ca8e
Michael R <54217603+Michael-R-R@users.noreply.github.com>
2023-06-22 18:04:38 -0700

    Added validation to contact me form;

--- Scripts/ContactMe/SubmitInfo.js
+++ Scripts/ContactMe/SubmitInfo.js
@@ -1,13 +1,19 @@
 "use strict";
 
-var nameInput = document.getElementById("name-input");
+var form = document.getElementById("input-form");
 var emailInput = document.getElementById("email-input");
-var textarea = document.getElementById("messagebox-input");
+var subjectInput = document.getElementById("subject-input");
+var msgInput = document.getElementById("messagebox-input");
 var submitButton = document.getElementById("submit-button");
 
 submitButton.onclick = onSubmit;
 
 function onSubmit(e)
 {
-    // Some how to message to my email
+    if(form.checkValidity() == true)
+    {
+        // TODO: Somehow send email
+
+        window.alert("Email sent");
+    }
 }
